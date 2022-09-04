1f29f1493f0f03aa27c493246104cfc159813770
HardikGupta21 <hardikgupta777@gmail.com>
2022-09-04 10:25:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './App.css';
+import { LoginForm } from './components/Loginform';
+import { SignUpForm } from './components/Signupform';
+// import { CSSTransition } from "react-transition-group";
+// import swal from 'sweetalert';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div className="main">
+          <Routes>
+            <Route exact path='/' element={<SignUpForm />}></Route>
+            <Route exact path='/loginform' element={<LoginForm />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
